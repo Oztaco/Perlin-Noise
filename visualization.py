3242eb7e33e89a3daf9cc1f630e3a2c90fb5ba81
Ari Bernstein <aribernstein@Aris-MacBook-Pro.local>
2021-02-23 20:37:43 -0500

    Added parameters for octives and labels on top of generated images

--- visualization.py
+++ visualization.py
@@ -2,9 +2,9 @@ import pandas as pd
 import numpy as np
 import perlin as per
 from matplotlib import cm
-from PIL import Image
+from PIL import Image, ImageDraw
 
-def perlin_dataframe(df_len:int, df_height:int) -> pd.DataFrame:
+def perlin_dataframe(df_len:int, df_height:int, num_octives:int, persistence:float) -> pd.DataFrame:
     perlin_obj = per.Perlin()
     perlin_df = pd.DataFrame(index=range(df_len),
                              columns=range(df_height))
@@ -13,18 +13,27 @@ def perlin_dataframe(df_len:int, df_height:int) -> pd.DataFrame:
     # perlin_df = perlin_df.applymap(lambda x: perlin_obj.perlin(x, x % , 0))
     for x in range(0, df_len):
         for y in  range(0, df_height):
-            perlin_df.iat[x,y] = perlin_obj.octive_perlin(x / 255, y / 255, 0.5, 4, .5)
+            perlin_df.iat[x,y] = perlin_obj.octive_perlin(x / 255, 
+                                                          y / 255, 
+                                                          0.5, 
+                                                          num_octives,
+                                                          persistence)
             
     return perlin_df
 
 
-def two_d_vis(img_len:int=500, img_height:int=1000):
-    perlin_df = perlin_dataframe(img_height, img_len)
+def two_d_vis(img_len:int=500, img_height:int=1000, num_octaves:int=1, persistence:int=1):
+    perlin_df = perlin_dataframe(img_height, img_len, num_octaves, persistence)
     print(perlin_df)
+    
     # Create base image    
     img = Image.fromarray(np.uint8(perlin_df.to_numpy()*255))
+    img_with_label = ImageDraw.Draw(img)
+    label_str = "Octaves: " + str(num_octaves) + "\nPersistence: " + str(persistence)
+
+    img_with_label.text((50,50), label_str)
     img.show()
 
-two_d_vis()
+two_d_vis(num_octaves=10, persistence=.5)
 
     
